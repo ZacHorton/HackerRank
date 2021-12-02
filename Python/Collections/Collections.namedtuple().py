81ee52bf4055b426d72876cb7f57191b18717ca4
Zac Horton <45723831+ZacHorton@users.noreply.github.com>
2021-12-01 21:24:42 -0500

    Update Collections.namedtuple().py

--- Python/Collections/Collections.namedtuple().py
+++ Python/Collections/Collections.namedtuple().py
@@ -1,11 +1,11 @@
 if __name__ == "__main__":
     n = int(input())
-    column_names, total, marks = input().split(), 0, 0
-    for i, x in enumerate(column_names):
-        if x == "MARKS":
-            marks = i
+    column_names = input().split()
+    marks_index = column_names.index("MARKS")
+    total = 0
     for x in range(n):
         data = input().split()
-        total += int(data[marks])
+        student_marks = int(data[marks_index])
+        total += student_marks
     print('{0:.2f}'.format(total / n))
     
