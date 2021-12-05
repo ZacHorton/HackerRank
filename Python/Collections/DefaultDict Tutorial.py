7d600c87a3627929a4cecc9086284eea5db0a325
Zac Horton <45723831+ZacHorton@users.noreply.github.com>
2021-12-05 14:31:20 -0500

    Update DefaultDict Tutorial.py

--- Python/Collections/DefaultDict Tutorial.py
+++ Python/Collections/DefaultDict Tutorial.py
@@ -1,24 +1,15 @@
 from collections import defaultdict
 
-
-if __name__ == '__main__':
-    nm = input().split(" ")
-    n = int(nm[0])
-    m = int(nm[1])
-    list_a, list_b = [], []
-    for i in range(n):
-        list_a.append(input())
-    for j in range(m):
-        list_b.append(input())
-
-    dd = defaultdict(list)
-    for i in range(len(list_b)):
-        for j in range(len(list_a)):
-            if list_b[i] == list_a[j]:
-                dd[list_b[i]].append(j + 1)
-        if list_b[i] not in list_a:
-            dd[list_b[i]].append(-1)
-    for key, value in dd.items():
-        for x in value:
-            print(x, end=" ")
-        print()
+if __name__ == "__main__":
+    user_input = list(map(int, input().split()))
+    n = user_input[0]
+    m = user_input[1]
+    group_a = [input() for i in range(n)]
+    group_b = [input() for i in range(m)]
+    d = defaultdict(list)
+    for i, x in enumerate(group_a):
+        d[x].append(i + 1)
+    d.default_factory = lambda: [-1]
+    for x in group_b:
+        print(*(d[x]))
+        
