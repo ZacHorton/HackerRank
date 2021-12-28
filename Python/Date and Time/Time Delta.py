1b7499ce7ad8c591bf07da5ceb082bc40a1e2b39
Zac Horton <45723831+ZacHorton@users.noreply.github.com>
2021-12-27 21:50:47 -0500

    Create Time Delta.py
    Did not pass all test cases.

---
+++ Python/Date and Time/Time Delta.py
@@ -0,0 +1,12 @@
+import datetime
+
+T = int(input())
+for i in range(T):
+  x1 = datetime.datetime.strptime(input(), '%a %d %b %Y %X %z')
+  x2 = datetime.datetime.strptime(input(), '%a %d %b %Y %X %z')
+  if x1 > x2:
+    return_value = (x1 - x2).total_seconds()
+    print(int(return_value))
+  else:
+    return_value = (x1 - x2).total_seconds()
+    print(int(return_value))
