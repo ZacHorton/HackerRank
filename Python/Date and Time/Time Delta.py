b84bd574fd5bb963a62d6fba7a736cdded7f0c8b
Zac Horton <45723831+ZacHorton@users.noreply.github.com>
2021-12-28 20:24:01 -0500

    Update Time Delta.py
    Resolved duplicate outcome inside else clause.

--- Python/Date and Time/Time Delta.py
+++ Python/Date and Time/Time Delta.py
@@ -1,12 +1,13 @@
 import datetime
 
-T = int(input())
-for i in range(T):
-  x1 = datetime.datetime.strptime(input(), '%a %d %b %Y %X %z')
-  x2 = datetime.datetime.strptime(input(), '%a %d %b %Y %X %z')
-  if x1 > x2:
-    return_value = (x1 - x2).total_seconds()
-    print(int(return_value))
-  else:
-    return_value = (x1 - x2).total_seconds()
-    print(int(return_value))
+if __name__ == "__main__":
+  T = int(input())
+  for i in range(T):
+    x1 = datetime.datetime.strptime(input(), '%a %d %b %Y %X %z')
+    x2 = datetime.datetime.strptime(input(), '%a %d %b %Y %X %z')
+    if x1 > x2:
+      return_value = (x1 - x2).total_seconds()
+      print(int(return_value))
+    else:
+      return_value = (x2 - x1).total_seconds()
+      print(int(return_value))
