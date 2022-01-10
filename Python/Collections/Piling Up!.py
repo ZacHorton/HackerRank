9fe18eb57ae6fef6c75a36185e7a49a177533ae1
Zac Horton <45723831+ZacHorton@users.noreply.github.com>
2022-01-09 23:34:53 -0500

    Update Piling Up!.py
    Rewrote algorithm to capture all test cases.

--- Python/Collections/Piling Up!.py
+++ Python/Collections/Piling Up!.py
@@ -5,24 +5,26 @@ if __name__ == "__main__":
   for i in range(T):
     n = int(input())
     blocks = deque(map(int, input().split()))
-    stack = 2**31
-    continues = True
-    while continues:
+    last = 2 ** 31
+    for j in range(n):
+      answer = "Unknown"
       left = blocks[0]
       right = blocks[-1]
-      if len(blocks) == 1 and left <= stack:
-        print("Yes")
-        continues = False  
-      elif left > right and left <= stack:
-        largest = left
-        stack = blocks.popleft()
-      elif left < right and right <= stack:
-        largest = right
-        stack = blocks.pop()
-      elif left == right and left <= stack:
+      if len(blocks) != 1 and left >= right:
+        last = left
+        blocks.popleft()
+      elif left < right and last >= right:
+        last = right
         blocks.pop()
-        stack = blocks.popleft()
-      else:
-        print("No")
-        continues = False
+      elif len(blocks) == 1:
+        if left > last:
+          answer = "No"
+          print(answer)
+        elif left <= last:
+          answer = "Yes"
+          print(answer)
+      elif len(blocks) != 1:
+          answer = "No"
+          print(answer)
+          break
   
